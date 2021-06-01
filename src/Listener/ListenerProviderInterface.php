e774f72f47e3700c8f1edda48b4ce46ccfcf0fd5
Anton Ukhanev <xedin.unknown@gmail.com>
2021-06-01 18:29:30 +0200

    Use dev version of PSR-14
    This allows the removal of `getListenersForEvent()`, because
    in the new version the typehint is compatible with static analyzers - see
    https://github.com/php-fig/event-dispatcher/commit/b97806eb70cd83152c5ceeeafeb79187dbee3da1.

--- src/Listener/ListenerProviderInterface.php
+++ src/Listener/ListenerProviderInterface.php
@@ -5,18 +5,10 @@ declare(strict_types=1);
 namespace Dhii\Events\Listener;
 
 use Psr\EventDispatcher\ListenerProviderInterface as PsrListenerProviderInterface;
-use Traversable;
-use Psr\EventDispatcher\StoppableEventInterface;
 
 /**
  * Temporary shadow.
  */
 interface ListenerProviderInterface extends PsrListenerProviderInterface
 {
-    /**
-     * @inheritDoc
-     *
-     * @return Traversable<callable(object|StoppableEventInterface)> A list of callables. Each callable MUST be type-compatible with $event.
-     */
-    public function getListenersForEvent(object $event): Traversable;
 }
